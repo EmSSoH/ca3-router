014276c3c536c08eb2ffacd50f27722350c83882
Magnus <cph-mm454@cphbusiness.dk>
2019-04-29 12:26:12 +0200

    fetch progress

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+let sw = null;
+ 
+fetch('https://swapi.co/api/people/1')
+  .then(response => response.json())
+  .then(json => {sw = json
+  })
+  .catch(e => { console.log('error!'); })
+
 const About = () => {
     return (
+      
       <div>
-        <p>About</p>
+        <p>text {sw}</p>
       </div>
     )
   };
